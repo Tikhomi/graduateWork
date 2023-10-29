1cea98143461f05176049584454973365ef68649
Tikhomi <tt.vv.aa.l00@gmail.com>
2023-10-30 00:58:02 +0400

    Initial commit

---
+++ src/test/java/com/example/graduateWork/GraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduateWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
