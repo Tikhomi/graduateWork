f4c523d456bb8e7f84d0a7dacaf50ec860c1bc31
Tikhomi <tt.vv.aa.l00@gmail.com>
2024-05-06 11:59:22 +0400

    minor changes

--- src/main/java/com/example/graduateWork/entity/Users.java
+++ src/main/java/com/example/graduateWork/entity/Users.java
@@ -3,7 +3,6 @@
     import lombok.AllArgsConstructor;
     import lombok.Data;
     import lombok.NoArgsConstructor;
-    import org.hibernate.validator.constraints.NotEmpty;
 
     import javax.persistence.*;
     import javax.validation.Valid;
